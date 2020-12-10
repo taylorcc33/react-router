1311701db8204ab88dccb97795f84734d3afb348
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-10 15:01:31 -0700

    made Nate.js and made sure what I write renders and displays correctly

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Container } from "semantic-ui-react";
+import Nate from './pages/Nate/Nate';
+import { Home, NoMatch } from './components/Navbar';
+import { Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Container>
-        {/* <Switch>
-          <Route exact path="/" component={Home} />
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
           <Route exact path="/nate" component={Nate} />
-          <Route exact path="/ian" component={Ian} />
-          <Route exact path="/rachel" component={Rachel} />
-          <Route exact path="/taylor" component={Taylor} />
-          <Route component={NoMatch} />
-        </Switch> */}
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
       </Container>
     </>
   );
---
+++ src/pages/Nate/Nate.js
@@ -0,0 +1,17 @@
+// so to block out everything I need to do, let's start with what I want
+
+// const { render } = require("@testing-library/react");
+
+// I WANT TO MAKE A PAGE THAT SHOWS GUITARS, AND YOU CAN CLICK TO BUY THEM AND PUT THEM IN YOUR 'CART' OR MARK THEM AS 'SOLD' OR 'LIKE/DISLIKE'
+
+// I need to make the page render first. Once I know the route is properly connected, I can start working on making a state to display a list of guitars
+
+
+const Nate = () => {
+
+  return (
+  <div>hello</div>
+  )
+}
+
+export default Nate;
